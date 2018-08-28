9a2ee871b8c248119ea202be92488be72ca72f78
Jess Lee <jessclee92@me.com>
2018-08-28 11:17:31 +0100

    removed routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-    resources :users
     resources :bookings
     resources :hostels, only: [:index, :show]
     resources :services, only: [:index, :show]
