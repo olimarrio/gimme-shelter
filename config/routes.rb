086558f278a44a5e8237cc3f2951181f69dca8a0
Ben Herring <ben_herring@hotmail.com>
2018-08-28 10:47:00 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+    resources :users
+    resources :bookings
+    resources :hostels, only: [:index, :show]
+    resources :services, only: [:index, :show]
+
+
+
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
