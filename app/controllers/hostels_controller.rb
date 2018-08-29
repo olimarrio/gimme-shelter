7b02ac3dd1201bee8377e91c5f961899afc2683d
Ben Herring <ben_herring@hotmail.com>
2018-08-29 18:11:16 +0100

    added search and autocomplete address

--- app/controllers/hostels_controller.rb
+++ app/controllers/hostels_controller.rb
@@ -2,15 +2,27 @@ class HostelsController < ApplicationController
 
   def index
     # @hostels = Hostel.all
-    # if params[:query].present?
-    #   @hostels = Hostel.search_address(params[:query]).where.not(latitude: nil, longitude: nil)
-    # else
-    #   @hostels = Hostel.where.not(latitude: nil, longitude: nil)
-    # end
-     if params[:query].present?
-      @hostels = Hostel.search_address(params[:query])
+    if params[:query].present?
+      @hostels = Hostel.search_address(params[:query]).where.not(latitude: nil, longitude: nil)
     else
-      @hostels = Hostel.all
+      @hostels = Hostel.where.not(latitude: nil, longitude: nil)
+    end
+
+    #  if params[:query].present?
+    #   @hostels = Hostel.search_address(params[:query])
+    # else
+    #   @hostels = Hostel.all
+    # end
+
+    # @hostels = Hostel.where.not(latitude: nil, longitude: nil)
+
+     @markers = @hostels.map do |hostel|
+      {
+        lat: hostel.latitude,
+        lng: hostel.longitude,
+        icon: { url: ActionController::Base.helpers.image_url('orangepin.png')}
+
+       }
     end
   end
 
