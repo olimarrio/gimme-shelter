8c6bac1220662df0d64322793f676f6191bc701f
Oliver Marriott <olimarrio@gmail.com>
2018-08-31 10:46:47 +0100

    complete seed file, no change to avatar

--- app/models/hostel.rb
+++ app/models/hostel.rb
@@ -4,8 +4,8 @@ class Hostel < ApplicationRecord
   has_many :bookings
   validates :name, presence: true
   validates :address, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
+  # validates :description, presence: true
+  # validates :price, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
