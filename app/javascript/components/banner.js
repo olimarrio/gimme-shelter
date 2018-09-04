a5eb4ee5ff4e5c094dd46fd43c78ec94a2230965
Jess Lee <jessclee92@me.com>
2018-09-04 13:20:46 +0100

    typed js

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,15 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["Welcome to Gimme Shelter", "Book a bed for those most in need today."],
+    typeSpeed: 40,
+    loop: false
+  });
+}
+
+export { loadDynamicBannerText };
+
+
+// <p>Welcome to Gimme Shelter. We provide a quick and effective way to book a hostel bed for those most in need.</p>
+// <p>Give shelter to a homeless person today</p>
